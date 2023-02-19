9c2c938f24b26e0c35a344411dd2d78099f3fc8d
VinayAvala <vinayav@infinite.com>
2023-02-19 10:50:31 +0530

    Add project files.

---
+++ Controllers/specializationController.cs
@@ -0,0 +1,42 @@
+﻿using InfiniteHealthCare.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace InfiniteHealthCare.Controllers
+{
+    public class SpecializationController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        public SpecializationController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(SpecializationViewModel specialization)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(_configuration["ApiUrl:api"]);
+                    var result = await client.PostAsJsonAsync("Specialization/CreateSpecialization", specialization);
+                    if (result.StatusCode == System.Net.HttpStatusCode.Created)
+                    {
+                        return RedirectToAction("Create", "Doctors");
+                    }
+                }
+            }
+            return View(specialization);
+        }
+    }
+}
---
+++ Models/AppointmentViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InfiniteHealthCare.Models
+{
+    public class AppointmentViewModel
+    {
+
+
+
+        //public int Id { get; set; }
+
+        //[Required(ErrorMessage = "Plese  enter Patient Name")]
+        //[Display(Name = "Patient Name")]
+
+        //public string PatientName { get; set; }
+
+        //[Required(ErrorMessage = "Plese enter Gender (Male-Female-Others)")]
+
+        //public string Gender { get; set; }
+
+        //[Required(ErrorMessage = "Plese enter the Age")]
+
+        //public int Age { get; set; }
+
+        //[Required(ErrorMessage = "Plese enter the Appointment Date")]
+        //[Display(Name = "Appointment Date")]
+        //public DateTime AppointmentDate { get; set; }
+
+        //[Required(ErrorMessage = "Plese provide the Specialization Of Doctor")]
+        //[Display(Name = "Specialization Of Doctor")]
+        //public string SpecializationOfDoctor { get; set; }
+
+        //[Required(ErrorMessage = "Plese provide the Time ")]
+        //[Display(Name = "Time ")]
+        //public DateTime Time { get; set; }
+
+
+        //[Required(ErrorMessage = "Plese provide the Doctor Name")]
+        //[Display(Name = "Doctor Name")]
+
+        //public string DoctorName { get; set; }
+
+
+        public int id { get; set; }
+
+       
+
+        public int Patientid { get; set; }
+
+
+        public int Doctorid { get; set; }
+
+        public string PatientName { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public DateTime AppointmentTime { get; set; }
+        public string Problem { get; set; }
+
+        public string specilization { get; set; }
+
+
+    }
+}
